02abf557d84225689400e73f90333500d0306fb4
unknown <delirioushoran@gmail.com>
2021-07-28 06:18:21 -0600

    Initial Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
 
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 
         <title>MMSU - Dorm Finder</title>
 
@@ -20,18 +21,17 @@
         
     <div class="uppernav"> <h3 style="margin-left:20px;color:#0C4B05;">MMSU </h3><h3> - Dorm Finder</h3> </div>
     <div class="topnav" id="myTopnav">
-        <img style="float:left;margin-left:20px;margin-top:10px;" src="/images/mmsu logo.png"  height="60" width="60">
+        <img style="float:left;margin-left:20px;" src="/images/mmsu logo.png"  height="5%" width="5%">
         <h4>MARIANO MARCOS <br> STATE UNIVERSITY</h4>
             <a href="#news">CONTACT</a>
             <a href="#contact">ABOUT US</a>
             <a href="dorm">LIST OF DORMS</a>
     </div>
     
-    <img style="float:left;margin-right:100px;margin-left:80px;" src="/images/mmsu logo.png"  height="620" width="614">
+    <img style="float:left;margin-right:100px;margin-left:80px;" src="/images/mmsu logo.png"  height="80%" width="40%">
 
-    <div class="container">
         <div class="logincontainer">
-            <form id="Login">
+            <form class="loginform">
                 <legend>Log In:</legend>
                     <input type="text" id="name" placeholder="Student ID" class="loginform" style="margin-top:30px;"> <br>
                     <input type="password" id="password" placeholder="Password" class="loginform" style="margin-top:10px;"> <br>
@@ -39,6 +39,6 @@
                     <a href="home"><button type="button" class="yellowbutton" style="margin-top:15px; margin-bottom:50px;">Log In</button></a> 
             </form>
         </div>
-    </div>
+  
     </body>
 </html>
