596dfb5c36542e1e961dd73414a9d5fc31e4d786
unknown <delirioushoran@gmail.com>
2021-08-02 06:11:45 -0600

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <link rel="icon" href="/images/mmsu logo.png">
     </head>
     
-    <body class="antialiased">
+    <body style="overflow: hidden;background-image: url('/images/bg.jpg');background-repeat: no-repeat; background-size: 100% 100%;" class="antialiased">
         
     <div class="uppernav"> <h3 style="margin-left:20px;color:#0C4B05;">MMSU </h3><h3> - Dorm Finder</h3> </div>
     <div class="topnav" id="myTopnav">
@@ -20,15 +20,15 @@
         <h4>MARIANO MARCOS <br> STATE UNIVERSITY</h4>
     </div>
     
-    <img style="float:left;margin-right:100px;margin-left:80px;" src="/images/mmsu logo.png"  height="80%" width="40%">
+    <img style="right:21%;" class="__img" src="images/mmsu logo.png" />
 
         <div class="logincontainer">
-            <form class="loginform">
+            <form>
                 <legend>Log In:</legend>
                     <input type="text" id="name" placeholder="Student ID" class="loginform" style="margin-top:30px;"> <br>
                     <input type="password" id="password" placeholder="Password" class="loginform" style="margin-top:10px;"> <br>
 
-                    <a href="home"><button type="button" class="yellowbutton" style="margin-top:15px; margin-bottom:50px;">Log In</button></a> 
+                    <a href="home"><button type="button" class="yellowbutton" style="margin-left:0%;margin-top:15px;">Log In</button></a> 
             </form>
         </div>
   
